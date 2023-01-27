400b83fb6729e03483337c24d68820b290388907
daryna-c <daryna@ualberta.ca>
2023-01-27 00:22:15 -0700

    complete version

--- README.md
+++ README.md
@@ -18,9 +18,38 @@ server.py contains contributions from:
 * Eddie Antonio Santos
 * Jackson Z Chang
 * Mandy Meindersma 
+* Daryna Chernyavska
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
+Other server.py references include:
+
+    Author: Mozilla Coporation and individual contributors
+    Title: HTTP
+    Year: 2023
+    Resource: https://developer.mozilla.org/en-US/docs/Web/HTTP
+    Attributions and copyright licensing by Mozilla Contributors is licensed under CC-BY-SA 2.5.
+
+    Authors: nikhilaggarwal3
+    Author's Page:
+        https://auth.geeksforgeeks.org/user/nikhilaggarwal3/articles
+    Improved by: surajkr_gupta https://auth.geeksforgeeks.org/user/surajkr_gupta,
+                 maityashis766 https://auth.geeksforgeeks.org/user/maityashis766
+    Title: Python: Check if a File or Directory Exists
+    Year: 2023
+    Resource: https://www.geeksforgeeks.org/python-check-if-a-file-or-directory-exists-2/
+    This resource is under creativecommons.org under CCBY-SA license
+
+    Author: Python Pool
+    Title: 5 Ways to Convert bytes to string in Python
+    Year: 2023
+    Resource: https://www.pythonpool.com/python-bytes-to-string/
+
+    OS path - Common pathnames manipulation was referenced which is © Copyright 2001-2023, Python Software Foundation
+    https://docs.python.org/3/library/os.path.html
+
+root.png and deep.png are created by Daryna Chernyavska
 
--- server.py
+++ server.py
@@ -23,6 +23,32 @@ import os.path
 #
 # http://docs.python.org/2/library/socketserver.html
 #
+# Other references include:
+#
+#    Author: Mozilla Coporation and individual contributors
+#    Title: HTTP
+#    Year: 2023
+#    Resource: https://developer.mozilla.org/en-US/docs/Web/HTTP
+#    Attributions and copyright licensing by Mozilla Contributors is licensed under CC-BY-SA 2.5.
+#
+#    Authors: nikhilaggarwal3
+#    Author's Page:
+#        https://auth.geeksforgeeks.org/user/nikhilaggarwal3/articles
+#    Improved by: surajkr_gupta https://auth.geeksforgeeks.org/user/surajkr_gupta,
+#                 maityashis766 https://auth.geeksforgeeks.org/user/maityashis766
+#    Title: Python: Check if a File or Directory Exists
+#    Year: 2023
+#    Resource: https://www.geeksforgeeks.org/python-check-if-a-file-or-directory-exists-2/
+#    This resource is under creativecommons.org under CCBY-SA license
+#
+#    Author: Python Pool
+#    Title: 5 Ways to Convert bytes to string in Python
+#    Year: 2023
+#    Resource: https://www.pythonpool.com/python-bytes-to-string/
+#
+#    OS path - Common pathnames manipulation was referenced which is © Copyright 2001-2023, Python Software Foundation
+#    https://docs.python.org/3/library/os.path.html
+#
 # run: python freetests.py
 
 # try: curl -v -X GET http://127.0.0.1:8080/
@@ -32,18 +58,15 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-#        print ("Got a request of: %s\n" % self.data)
-        response = self.parseRequest()
+        print ("Got a request of: %s\n" % self.data)
+        response = self.createResponse()
         self.request.sendall(bytearray(response,'utf-8'))
-        #print("response:\n", response)
     
     def getPath(self):
+        # parses the request to obtain the path provided and uses it to create the respective local path 
         strData = self.data.decode()
         path = strData[strData.index("/") : strData.index(" HTTP/")]
-#        print("path:", path)
-        #localPath = os.path.join(os.getcwd(), "/www"+path)
         localPath = os.getcwd() + "/www" + path 
-#        print("local path:", localPath)
         return path, localPath
 
     def checkIsSafePath(self, desired):
@@ -55,45 +78,48 @@ class MyWebServer(socketserver.BaseRequestHandler):
             return False
 
     def determineFileType(self, path):
+        # checks the extension of the file and returns the appropriate line for content type for the HTTP header
         basename = os.path.basename(path)
         print("basename:", basename)
         if basename.find(".css") != -1:
-            #print("ContentType: text/css")
             return "Content-Type: text/css; charset=utf-8\r\n"
         elif basename.find(".html") != -1:
-            #print("ContentType: text/html")
             return "Content-Type: text/html; charset=utf-8\r\n"
         else:
             return ""
 
-    def parseRequest(self):
-        response = "NO RESPONSE!!!"
-        if self.data.startswith(b"GET ") or self.data.startswith(b"HEAD "):
+    def createResponse(self):
+        # parses the request and returns an appropriate response 
+        if self.data.startswith(b"GET "): 
             path, localPath = self.getPath()
             if not self.checkIsSafePath(localPath):
                 response = "HTTP/1.1 404 Not Found\r\n"
             else:
                 if os.path.isfile(localPath):
+                    # handles an existing file
                     file = open(localPath, "r")
                     fileContent = file.read()
                     file.close()
                     contentType = self.determineFileType(path)
                     response = "HTTP/1.1 200 OK\r\n{}\n{}\r\n".format(contentType, fileContent)
                 elif os.path.isdir(localPath):
-                    if localPath[len(localPath)-1] != "/":
+                    # handles an existing directory
+                    if localPath[len(localPath)-1] != "/": 
+                        # handles a directory that exists but a correct path was not provided
                         response = "HTTP/1.1 301 Moved Permanently\r\nLocation: {}/\r\n".format(path)
                     else:
+                        # handles an existing directory with a correct path
                         try:
                             file = open(localPath+"index.html", "r")
                             fileContent = file.read()
                             file.close()
                             response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\n{}\r\n".format(fileContent)
-                        except OSError:
+                        except OSError: # index.html file does not exist for this directory
                             response = "HTTP/1.1 404 Not Found\r\n"
 
-                else:
+                else: # no such file or directory
                     response = "HTTP/1.1 404 Not Found\r\n"
-        else:
+        else: # method not handled
             response = "HTTP/1.1 405 Method Not Allowed\r\n"
         
         return response
